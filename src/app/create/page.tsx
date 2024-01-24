dac4d183a89df27692db84de1d4578f7f6135eed
Arthur Faliy <faliyarthur@gmail.com>
2024-01-24 19:26:20 +0200

    Fix production build again

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,10 +1,15 @@
 import UpsertMovieForm from '@/app/components/UpsertMovieForm'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Create / Edit movie',
 }
 
 export default function CreateMoviePage() {
-  return <UpsertMovieForm />
+  return (
+    <Suspense>
+      <UpsertMovieForm />
+    </Suspense>
+  )
 }
